03fc26adadb6967665a80207113910ce07e0165b
ichuksin <i.chuksin@gmail.com>
2020-10-05 22:53:07 +0300

    Initial commit

---
+++ Scripts/Shop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private List<Weapon> _weapons;
+    [SerializeField] private Player _player;
+    [SerializeField] private WeaponView _template;
+    [SerializeField] private GameObject _itemContainer;
+
+    private void Start()
+    {
+        for (int i = 0; i < _weapons.Count; i++)
+        {
+            if (_player.HaveWeapon(_weapons[i]))
+                _weapons[i].SetBuyed(true);
+            else
+                _weapons[i].SetBuyed(false);
+            AddItem(_weapons[i]);
+        }
+    }
+
+    private void AddItem(Weapon weapon)
+    {
+        var view = Instantiate(_template, _itemContainer.transform);
+        view.SellButtonClick += OnSellButtonClick;
+        view.Render(weapon);
+    }
+
+    private void OnSellButtonClick(Weapon weapon, WeaponView view)
+    {
+        TrySellWeapon(weapon, view);
+    }
+
+    private void TrySellWeapon(Weapon weapon, WeaponView view)
+    {
+        if(weapon.Price <= _player.Money)
+        {
+            _player.BuyWeapon(weapon);
+            weapon.SetBuyed(true);
+            view.SellButtonClick -= OnSellButtonClick;
+        }
+    }
+}
