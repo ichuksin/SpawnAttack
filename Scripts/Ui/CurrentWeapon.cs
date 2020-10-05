96e583de0bf566cf4b98dec8278ab2b0de316139
ichuksin <i.chuksin@gmail.com>
2020-10-05 22:56:22 +0300

    Init
    Init

---
+++ Scripts/Ui/CurrentWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CurrentWeapon : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private Image _weaponIcon;
+
+    private void OnEnable()
+    {
+        _player.WeaponChanged += ShowWeapon;
+    }
+    private void OnDisable()
+    {
+        _player.WeaponChanged -= ShowWeapon;
+    }
+    private void ShowWeapon(Weapon weapon)
+    {
+        _weaponIcon.sprite = weapon.Icon;
+    }
+}
